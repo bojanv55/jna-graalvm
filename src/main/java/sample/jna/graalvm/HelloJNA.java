dbd755f335e58503c4670443c2c37f260aa5c25c
b.vukasovic <b.vukasovic@sportradar.com>
2021-10-07 22:39:33 +0200

    added oshi

--- src/main/java/sample/jna/graalvm/HelloJNA.java
+++ src/main/java/sample/jna/graalvm/HelloJNA.java
@@ -1,6 +1,11 @@
 package sample.jna.graalvm;
 
 import com.sun.jna.Native;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.ComputerSystem;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.software.os.OperatingSystem;
 
 public class HelloJNA {
 	
@@ -8,6 +13,29 @@ public class HelloJNA {
 		
 		System.out.println("Hello there!");
 		System.out.println("JNA Version::" + Native.VERSION);
+
+		SystemInfo systemInfo = new SystemInfo();
+		OperatingSystem operatingSystem = systemInfo.getOperatingSystem();
+		HardwareAbstractionLayer hardwareAbstractionLayer = systemInfo.getHardware();
+		CentralProcessor centralProcessor = hardwareAbstractionLayer.getProcessor();
+		ComputerSystem computerSystem = hardwareAbstractionLayer.getComputerSystem();
+
+		String vendor = operatingSystem.getManufacturer();
+		String processorSerialNumber = computerSystem.getSerialNumber();
+		String processorIdentifier = centralProcessor.getProcessorIdentifier().getProcessorID();
+		int processors = centralProcessor.getLogicalProcessorCount();
+
+		String delimiter = "#";
+
+		String all = vendor +
+				delimiter +
+				processorSerialNumber +
+				delimiter +
+				processorIdentifier +
+				delimiter +
+				processors;
+
+		System.out.println("Sve je " + all);
 	}
 
 }
